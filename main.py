716b7533032d9a7d04faa98cf752adb9dc36fc94
Dzimmii <radim.hurda@seznam.cz>
2026-09-09 23:52:36 +0200

    doplnena moznost psat bez diakritiky

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import json
+import unicodedata
+
+def odstran_diakritiku(text):
+    return ''.join(
+        znak for znak in unicodedata.normalize('NFKD', text)
+        if not unicodedata.combining(znak)
+    )
 
 with open("diagnozy.json", "r", encoding="utf8") as soubor:
     diagnozy = json.load(soubor)
 
-hledane_slovo = input("Zadej název diagnózy: ")
+hledane_slovo = odstran_diakritiku(input("Zadej název diagnózy: ").lower())
 nalezeno = False
 
 for kod, nazev in diagnozy.items():
-    if hledane_slovo.lower() in nazev.lower():
-        print(f"{kod}   {nazev}")
+    if hledane_slovo in odstran_diakritiku(nazev.lower()):
+        print(f"{kod:<8}   {nazev}")
         nalezeno = True
 
 if not nalezeno:
